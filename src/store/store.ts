a6a839a60728ce7626ae4492bf03ec09747fc9c3
nzkks <kkshanthosh@gmail.com>
2024-07-09 23:39:39 +1200

    Create cart slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,9 +3,11 @@ import { immer } from 'zustand/middleware/immer';
 
 import { Store } from '@/types/store';
 import { createUserSlice } from '@/store/user-slice';
+import { createCartSlice } from '@/store/cart-slice';
 
 export const useStore = create<Store>()(
   immer((...a) => ({
     ...createUserSlice(...a),
+    ...createCartSlice(...a),
   }))
 );
--- src/types/store.ts
+++ src/types/store.ts
@@ -1,3 +1,4 @@
+import { CartSlice } from '@/store/cart-slice';
 import { UserSlice } from '@/store/user-slice';
 
-export type Store = UserSlice;
+export type Store = UserSlice & CartSlice;
