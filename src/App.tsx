9a8f121c68547863af85ff47c319b1d4c9248946
nzkks <kkshanthosh@gmail.com>
2024-09-03 20:20:05 +1200

    App.tsx: map the newly added mock product data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { PRODUCTS_DATA } from '@/lib/mockData';
+
 const App = () => {
-  return <></>;
+  return (
+    <main className="space-y-2 dark h-screen bg-background max-w-sm mx-auto mt-2">
+      <h1 className="text-2xl">Products:</h1>
+      <div className="space-y-2">
+        {PRODUCTS_DATA.map(product => (
+          <div key={product.id}>{product.title}</div>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default App;
