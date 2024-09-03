cd177d24afe91b12c281ac47740fdc9122befd86
nzkks <kkshanthosh@gmail.com>
2024-09-03 21:55:55 +1200

    store.ts: integrate devtools middleware from Zustand to view the whats going on in the browser by using Redux devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,16 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
+import { devtools } from 'zustand/middleware';
 
 import { Store } from '@/types/store';
 import { createUserSlice } from '@/store/user-slice';
 import { createCartSlice } from '@/store/cart-slice';
 
 export const useStore = create<Store>()(
-  immer((...a) => ({
-    ...createUserSlice(...a),
-    ...createCartSlice(...a),
-  }))
+  devtools(
+    immer((...a) => ({
+      ...createUserSlice(...a),
+      ...createCartSlice(...a),
+    }))
+  )
 );
